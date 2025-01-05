c999338eb6f7053722f71f1d184574c511d167d7
Nuthanan Tharmarajah <nuthanan06@gmail.com>
2025-01-04 22:22:17 -0500

    json and js file changes

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,6 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
-      '/cohere': {
-        target: 'http://127.0.0.1:5000/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/cohere/, ""),
-
-      }
     }
   }
 
